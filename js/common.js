01981c78eb2ce599a7713d37bc2a62266502c13d
khashashin <khashashin@gmail.com>
2017-03-27 23:53:55 +0200

    last changes
    urok zakonchilsya

--- js/common.js
+++ js/common.js
@@ -4,11 +4,16 @@ $(document).ready(function() {
         $(this).after("<div class='mc_item_wrap_af'></div>");
     });
     $(".mc_toggle").click(function() {
+			if($(this).parent().parent().children("ul").is(":visible")) {
+				$(this).parent().parent().children("ul").slideUp();
+				$(this).parent().parent().children(".mc_item_wrap_af").hide();
+			} else {
         $("body .mc_wrap .mc_item_wrap > ul, .mc_item_wrap_af").hide();
 				$(".mc_item_wrap").removeClass("active");
         $(this).parent().parent().addClass("active");
-        $(this).parent().parent().children("ul").show();
-        $(this).parent().parent().children(".mc_item_wrap_af").show(0);
+        $(this).parent().parent().children("ul").slideDown();
+        $(this).parent().parent().children(".mc_item_wrap_af").show();
+			}
     });
 
     //Цели для Яндекс.Метрики и Google Analytics
@@ -29,7 +34,7 @@ $(document).ready(function() {
     //equalheight - одинаковая высота колонок
     //Пример списка элементов:
     //var eqElement = ".cat_container > div, .home_news > div"
-    var eqElement = ".element"
+    var eqElement = ".hi_item, .sb_content > div"
     $(window).load(function() {
         equalheight(eqElement);
     }).resize(function() {
